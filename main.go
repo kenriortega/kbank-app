dd510961d97a31155b576e99f2458c6a46d2dd6e
kenriortega <kenriortega@gmail.com>
2021-06-23 10:55:38 -0500

    feat: add json tags and content-type

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"full_name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zip_code"`
+}
+
 func main() {
 
 	// define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	// starting server
 	log.Fatal(http.ListenAndServe(":8000", nil))
@@ -18,4 +26,13 @@ func main() {
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
+}
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "C1", City: "Hav", Zipcode: "12222"},
+		{Name: "C2", City: "Mtz", Zipcode: "343434"},
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
