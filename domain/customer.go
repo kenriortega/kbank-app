22ae6c562fb9d53063be4b5af1def7798cd7579e
kenriortega <kenriortega@gmail.com>
2021-06-24 14:25:56 -0400

    refactor: add tags template for json and bson data types and custom error types

--- domain/customer.go
+++ domain/customer.go
@@ -1,14 +1,16 @@
 package domain
 
+import "github.org/kbank/errs"
+
 type Customer struct {
-	ID          string
-	Name        string
-	City        string
-	Zipcode     string
-	DateofBirth string
-	Status      string
+	ID          string `json:"_id" bson:"_id"`
+	Name        string `json:"name" bson:"name"`
+	City        string `json:"city" bson:"city"`
+	Zipcode     string `json:"zip_code" bson:"zip_code"`
+	DateofBirth string `json:"date_of_birth" bson:"date_of_birth"`
+	Status      string `json:"status" bson:"status"`
 }
 
 type CustomerRepository interface {
-	FindAll() ([]Customer, error)
+	FindAll() ([]Customer, *errs.AppError)
 }
