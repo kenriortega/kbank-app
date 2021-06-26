0467364ccc43a60119751de72d99401067d49372
kenriortega <kenriortega@gmail.com>
2021-06-26 17:04:11 -0400

    feat: add createOne method to register new customer

--- customer/domain/customerRepositoryDb.go
+++ customer/domain/customerRepositoryDb.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"os"
 	"sync"
+	"time"
 
 	"github.org/kbank/internal/errs"
 	"github.org/kbank/internal/logger"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -60,7 +62,25 @@ func (d CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError
 
 }
 
-//GetMongoClient - Return mongodb connection to work with
+//CreateOne - Insert a new document in the collection.
+func (d CustomerRepositoryDb) CreateOne(customer Customer) (*mongo.InsertOneResult, *errs.AppError) {
+	customer.ID = primitive.NewObjectID()
+	customer.CreatedAt = time.Now()
+	customer.UpdatedAt = time.Now()
+	customer.Status = "inactive"
+	//Create a handle to the respective collection in the database.
+	collection := d.client.Database(DB).Collection(CUSTOMERS)
+	//Perform InsertOne operation & validate against the error.
+	result, err := collection.InsertOne(context.TODO(), customer)
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, errs.NewUnexpectedError("Unexpected error on insert document")
+	}
+	//Return success without any error.
+	return result, nil
+}
+
+//NewCustomerRepositoryDb - Return mongodb connection to work with
 func NewCustomerRepositoryDb() CustomerRepositoryDb {
 	var clientInstance *mongo.Client
 	//Perform connection creation operation only once.
