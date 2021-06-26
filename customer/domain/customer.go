a915a36b4fb0f99d98412d312b0b0921dbcda7f4
kenriortega <kenriortega@gmail.com>
2021-06-26 17:03:29 -0400

    refactor: add objectID for id customener in mongodb

--- customer/domain/customer.go
+++ customer/domain/customer.go
@@ -5,17 +5,19 @@ import (
 
 	dto "github.org/kbank/customer/dto"
 	"github.org/kbank/internal/errs"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Customer struct {
-	ID          string    `bson:"_id"`
-	Name        string    `bson:"name"`
-	City        string    `bson:"city"`
-	Zipcode     string    `bson:"zip_code"`
-	DateofBirth string    `bson:"date_of_birth"`
-	Status      string    `bson:"status"`
-	CreatedAt   time.Time `bson:"created_at"`
-	UpdatedAt   time.Time `bson:"updated_at"`
+	ID          primitive.ObjectID `bson:"_id"`
+	Name        string             `bson:"name"`
+	City        string             `bson:"city"`
+	Zipcode     string             `bson:"zip_code"`
+	DateofBirth string             `bson:"date_of_birth"`
+	Status      string             `bson:"status"`
+	CreatedAt   time.Time          `bson:"created_at"`
+	UpdatedAt   time.Time          `bson:"updated_at"`
 }
 
 func (c Customer) ToDto() dto.CustomerResponse {
@@ -26,9 +28,12 @@ func (c Customer) ToDto() dto.CustomerResponse {
 		Zipcode:     c.Zipcode,
 		DateofBirth: c.DateofBirth,
 		Status:      c.Status,
+		CreatedAt:   c.CreatedAt,
+		UpdatedAt:   c.UpdatedAt,
 	}
 }
 
 type CustomerRepository interface {
 	FindAll(status string) ([]Customer, *errs.AppError)
+	CreateOne(Customer) (*mongo.InsertOneResult, *errs.AppError)
 }
