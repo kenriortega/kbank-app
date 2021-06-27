eb1d173fdc8033193564967ea703447b8831e7b9
kenriortega <kenriortega@gmail.com>
2021-06-27 11:37:01 -0400

    refactor: groupby routes all customers routes, improve server and usage cors and loggin middleware

--- cmd/api/app.go
+++ cmd/api/app.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
@@ -24,14 +25,26 @@ func Start() {
 	ch := handlers.CustomerHandler{
 		Service: service.NewCustomerService(domain.NewCustomerRepositoryDb()),
 	}
-	// define routes
-	r.HandleFunc("/customers/{customerID}/status", ch.UpdateStatusCustomer).Methods(http.MethodPatch)
-	r.HandleFunc("/customers/{customerID}", ch.DeleteCustomer).Methods(http.MethodDelete)
-	r.HandleFunc("/customers/{customerID}", ch.GetCustomer).Methods(http.MethodGet)
-	r.HandleFunc("/customers", ch.GetAllCustomers).Methods(http.MethodGet)
-	r.HandleFunc("/customers", ch.CreateCustomer).Methods(http.MethodPost)
+	// define routes for customers
+	customersRoutes := r.PathPrefix("/customers").Subrouter()
+	customersRoutes.HandleFunc("/{customerID}/status", ch.UpdateStatusCustomer).Methods(http.MethodPatch)
+	customersRoutes.HandleFunc("/{customerID}", ch.DeleteCustomer).Methods(http.MethodDelete)
+	customersRoutes.HandleFunc("/{customerID}", ch.GetCustomer).Methods(http.MethodGet)
+	customersRoutes.HandleFunc("/", ch.GetAllCustomers).Methods(http.MethodGet)
+	customersRoutes.HandleFunc("/", ch.CreateCustomer).Methods(http.MethodPost)
 
+	// middleware
+	r.Use(LogginMiddleware)
+	r.Use(mux.CORSMethodMiddleware(r))
 	// starting server
-	err = http.ListenAndServe(fmt.Sprintf(":%s", port), r)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    fmt.Sprintf("0.0.0.0:%s", port),
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+	}
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 }
