f8d69e5b3850e2be2d98655cd93b51890a2df849
kenriortega <kenriortega@gmail.com>
2021-06-27 09:38:46 -0400

    feat: impl all ports in the adpater

--- customer/domain/customerRepositoryDb.go
+++ customer/domain/customerRepositoryDb.go
@@ -33,7 +33,7 @@ func (d CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError
 		//Define filter query for fetching specific document from collection
 		filter = bson.D{{}} //bson.D{{}} specifies 'all documents'
 	} else {
-		// TODO: make filter query
+		filter = bson.D{primitive.E{Key: "status", Value: status}}
 	}
 	var customers []Customer
 	collection := d.client.Database(DB).Collection(CUSTOMERS)
@@ -72,6 +72,90 @@ func (d CustomerRepositoryDb) CreateOne(customer Customer) (*mongo.InsertOneResu
 	collection := d.client.Database(DB).Collection(CUSTOMERS)
 	//Perform InsertOne operation & validate against the error.
 	result, err := collection.InsertOne(context.TODO(), customer)
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, errs.InsertOneError
+	}
+	//Return success without any error.
+	return result, nil
+}
+
+//CreateMany - Insert multiple documents at once in the collection.
+func (d CustomerRepositoryDb) CreateMany(list []Customer) (*mongo.InsertManyResult, *errs.AppError) {
+	//Map struct slice to interface slice as InsertMany accepts interface slice as parameter
+	insertableList := make([]interface{}, len(list))
+	for i, v := range list {
+		insertableList[i] = v
+	}
+
+	//Create a handle to the respective collection in the database.
+	collection := d.client.Database(DB).Collection(CUSTOMERS)
+	//Perform InsertMany operation & validate against the error.
+	result, err := collection.InsertMany(context.TODO(), insertableList)
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, errs.InsertOneError
+	}
+	//Return success without any error.
+	return result, nil
+}
+
+//DeleteOne - Get customer by id for collection
+func (d CustomerRepositoryDb) DeleteOne(customerID primitive.ObjectID) (*mongo.DeleteResult, *errs.AppError) {
+	//Define filter query for fetching specific document from collection
+	filter := bson.D{primitive.E{Key: "_id", Value: customerID}}
+	//Get MongoDB connection using
+
+	//Create a handle to the respective collection in the database.
+	collection := d.client.Database(DB).Collection(CUSTOMERS)
+
+	//Perform DeleteOne operation & validate against the error.
+	result, err := collection.DeleteOne(context.TODO(), filter)
+
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, errs.InsertOneError
+	}
+	//Return success without any error.
+	return result, nil
+}
+
+//DeleteAll - Get All issues for collection
+func (d CustomerRepositoryDb) DeleteAll() (*mongo.DeleteResult, *errs.AppError) {
+	//Define filter query for fetching specific document from collection
+	selector := bson.D{{}} // bson.D{{}} specifies 'all documents'
+
+	//Create a handle to the respective collection in the database.
+	collection := d.client.Database(DB).Collection(CUSTOMERS)
+	//Perform DeleteMany operation & validate against the error.
+	result, err := collection.DeleteMany(context.TODO(), selector)
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, errs.InsertOneError
+	}
+	//Return success without any error.
+	return result, nil
+}
+
+func (d CustomerRepositoryDb) UpdateStatusByCustomerID(
+	customerID primitive.ObjectID,
+	status string,
+	updatedAt time.Time,
+) (*mongo.UpdateResult, *errs.AppError) {
+	//Define filter query for fetching specific document from collection
+	filter := bson.D{primitive.E{Key: "_id", Value: customerID}}
+
+	//Define updater for to specifiy change to be updated.
+	updater := bson.D{primitive.E{Key: "$set", Value: bson.D{
+		primitive.E{Key: "status", Value: status},
+		primitive.E{Key: "updated_at", Value: updatedAt},
+	}}}
+
+	collection := d.client.Database(DB).Collection(CUSTOMERS)
+
+	//Perform UpdateOne operation & validate against the error.
+	result, err := collection.UpdateOne(context.TODO(), filter, updater)
+
 	if err != nil {
 		logger.Error(err.Error())
 		return nil, errs.InsertOneError
